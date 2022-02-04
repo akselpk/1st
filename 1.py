983e1f0f976525f75bb540ce2bc9e7dd83d8efa4
akselpk <akselpk41@gmail.com>
2022-02-04 17:33:07 +0300

    123

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
-def adds_numbers (number:int=1) -> int:
-    '''Takes a number (default value is 1), and adds 2'''
-    return number + 2
+class Computing:
+    '''Class for make a compute'''
+    
+    def adds_numbers(number:int=1) -> int:
+        '''Takes a number (default value is 1), and adds 2'''
+        return number + 2
 
 
 print(adds_numbers(400))
